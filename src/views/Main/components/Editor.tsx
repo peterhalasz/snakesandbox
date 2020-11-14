5c9f8b201fe1dbd6def51bb14613757b65ce394f
Peter Halasz <peter.halasz@live.com>
2020-11-14 18:50:00 +0100

    feat: remove editor gutter

--- src/views/Main/components/Editor.tsx
+++ src/views/Main/components/Editor.tsx
@@ -9,19 +9,18 @@ interface Props {
   onCodeChange: (code: string) => void;
 }
 
-export default class Editor extends React.Component<Props> {
-  render() {
-    return (
-      <AceEditor
-        onChange={this.props.onCodeChange}
-        mode="javascript"
-        theme="chrome"
-        name="editor"
-        fontSize={14}
-        value={this.props.code}
-        style={{ float: "left", height: "93vh", width: "100%" }}
-        showPrintMargin={false}
-      />
-    );
-  }
+export default function Editor({ onCodeChange, code }: Props) {
+  return (
+    <AceEditor
+      onChange={onCodeChange}
+      mode="javascript"
+      theme="chrome"
+      name="editor"
+      fontSize={14}
+      value={code}
+      style={{ float: "left", height: "93vh", width: "100%" }}
+      showPrintMargin={false}
+      showGutter={false}
+    />
+  );
 }
